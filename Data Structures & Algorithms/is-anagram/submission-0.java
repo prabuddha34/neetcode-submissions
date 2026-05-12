86b1fcbec6d4df8f71a1e78c349817c9de1485a7
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-05-12 12:02:10 +0530

    Bulk sync: 12 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+         }
+         int c[]=new int[26];
+         for(int i=0;i<s.length();i++){
+            c[s.charAt(i)-'a']++;
+            c[t.charAt(i)-'a']--;
+         }
+         for(int count:c){
+            if(count!=0)
+            return false;
+         }
+         return true;
+    }
+}
