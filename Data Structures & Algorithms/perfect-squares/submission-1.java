8460442f1fac23786eb279c5372ced8d27057f9d
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-06-05 08:05:53 +0530

    Add: perfect-squares - submission-1

---
+++ Data Structures & Algorithms/perfect-squares/submission-1.java
@@ -0,0 +1,21 @@
+
+
+class Solution {
+    public int numSquares(int n) {
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, n);
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int s = 1; s * s <= i; s++) {
+                int square = s * s;
+
+                dp[i] = Math.min(dp[i], 1 + dp[i - square]);
+            }
+        }
+
+        return dp[n];
+    }
+}
