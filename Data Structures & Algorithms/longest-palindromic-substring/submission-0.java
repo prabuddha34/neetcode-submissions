a5df5c5326ee0868d3612e326a36648b1c7fcf36
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-06-08 07:35:35 +0530

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        if (n == 0) return "";
+
+        boolean[][] dp = new boolean[n][n];
+
+        int start = 0;
+        int maxLen = 1;
+
+        // single letters
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        // check all lengths
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (len == 2 || dp[i + 1][j - 1]) {
+                        dp[i][j] = true;
+
+                        if (len > maxLen) {
+                            maxLen = len;
+                            start = i;
+                        }
+                    }
+                }
+            }
+        }
+
+        return s.substring(start, start + maxLen);
+    }
+}
