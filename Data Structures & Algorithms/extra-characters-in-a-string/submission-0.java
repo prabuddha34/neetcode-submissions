0a06cc845ffa13d43f8f816bfc51c2e1fcb56524
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-09-02 22:31:08 +0530

    Add: extra-characters-in-a-string - submission-0

---
+++ Data Structures & Algorithms/extra-characters-in-a-string/submission-0.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int minExtraChar(String s, String[] dictionary) {
+        Set<String> words = new HashSet<>(Arrays.asList(dictionary));
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        dp[n] = 0;
+
+        return dfs(0, s, words, dp);
+    }
+
+    private int dfs(int i, String s, Set<String> words, int[] dp) {
+        if (dp[i] != -1) return dp[i];
+        int res = 1 + dfs(i + 1, s, words, dp);
+        for (int j = i; j < s.length(); j++) {
+            if (words.contains(s.substring(i, j + 1))) {
+                res = Math.min(res, dfs(j + 1, s, words, dp));
+            }
+        }
+        dp[i] = res;
+        return res;
+    }
+}
